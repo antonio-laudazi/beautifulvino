271854d981e6fcb431799a14890881e7ead1dcca
LaganaDavide <37578164+LaganaDavide@users.noreply.github.com>
2018-04-23 11:45:05 +0200

    put image bug

--- FunzioniLambda/Marte5BeautifulVinoFunzioniLambda/src/main/java/com/amazonaws/lambda/funzioni/put/putImageGen.java
+++ FunzioniLambda/Marte5BeautifulVinoFunzioniLambda/src/main/java/com/amazonaws/lambda/funzioni/put/putImageGen.java
@@ -82,7 +82,7 @@ public class putImageGen implements RequestHandler<RichiestaPutGenerica, Rispost
         AmazonS3 client = null;
         
         try {
-        		client = AmazonS3ClientBuilder.standard().build();
+        		client = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
 		} catch (Exception e1) {
 			esito.setCodice(EsitoHelper.ESITO_KO_CODICE_ERRORE_SALVATAGGIO);
 			esito.setMessage(EsitoHelper.ESITO_KO_MESSAGGIO_ERRORE_PROCEDURA_LAMBDA + " putImage ");
